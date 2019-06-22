48a390febb998038861c6c484f6bbeda0136f49c
chlwhdtn03 <chlwhdtn03@naver.com>
2019-06-22 21:13:50 +0900

    주석 추가

--- app/src/main/java/com/example/git/MainActivity.java
+++ app/src/main/java/com/example/git/MainActivity.java
@@ -4,7 +4,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
-
+    //앱이 실행될때
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
